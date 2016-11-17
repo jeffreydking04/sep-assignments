0e1938a1dff3f8c34f74f845d5d62df53dc4c11e
Jeffrey King <jdking33@aol.com>
2016-11-17 11:53:13 -0800

    Submit Data Structures-Checkpoint 2-Stacks and Queues

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -4,15 +4,34 @@ class MyQueue
 
   def initialize
     @queue = Array.new
-    @head = @queue[0]
+    @head = nil
+    @tail = nil
+    @head_pointer = -1
+    @tail_pointer = 0
   end
 
   def enqueue(element)
+    @tail = element if empty?
+    @head_pointer += 1
+    @queue[@head_pointer] = element
+    @head = element
   end
 
   def dequeue
+    temp = @tail
+    @tail_pointer += 1
+    if empty?
+      @head_pointer = -1
+      @tail_pointer = 0
+      @head = nil
+      @tail = nil
+    else
+      @tail = @queue[@tail_pointer]
+    end
+    temp
   end
 
   def empty?
+    @tail_pointer > @head_pointer
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -4,14 +4,23 @@ class MyStack
   def initialize
     @stack = Array.new
     self.top = nil
+    @index = -1
   end
 
   def push(item)
+    @index += 1
+    @stack[@index] = item
+    self.top = item
   end
 
   def pop
+    temp = @stack[@index]
+    @index -= 1
+    empty? ? self.top = nil : self.top = @stack[@index]
+    temp
   end
 
   def empty?
+    @index == -1
   end
 end
